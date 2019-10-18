0249f0243f0380ff743f9efa2d1450330fbd2992
Tijana Vislavski <tijana.vislavski@kolotree.com>
2019-10-18 20:26:26 +0200

    Moved exception resolving to separate class. Added logging

--- src/main/java/com/kolotree/springproductmanagement/web/ProductController.java
+++ src/main/java/com/kolotree/springproductmanagement/web/ProductController.java
@@ -3,18 +3,19 @@ package com.kolotree.springproductmanagement.web;
 import com.kolotree.springproductmanagement.domain.SKU;
 import com.kolotree.springproductmanagement.dto.ProductDto;
 import com.kolotree.springproductmanagement.ports.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController(value = "/product")
+@RestController
 public class ProductController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private final ProductRepository productRepository;
 
     @Autowired
@@ -22,30 +23,29 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    @GetMapping
+    @GetMapping("/product")
     public ResponseEntity<List<ProductDto>> getAll() {
-        return ResponseEntity.ok(productRepository.getAll().stream().map(ProductDto::from).collect(Collectors.toList()));
+        LOGGER.debug("Executing getAll products...");
+        var response = ResponseEntity.ok(productRepository.getAll().stream().map(ProductDto::from).collect(Collectors.toList()));
+        LOGGER.debug("Getting all products finished");
+        return response;
     }
 
-    @PostMapping
+    @PostMapping("/product")
     public ResponseEntity<ProductDto> save(@RequestBody ProductDto productDto) {
+        LOGGER.debug("Executing save product {}...", productDto);
         productDto = ProductDto.from(productRepository.save(productDto.toDomain()));
-        return ResponseEntity.ok(productDto);
+        var response = ResponseEntity.ok(productDto);
+        LOGGER.debug("Saving of product {} finished", productDto);
+        return response;
     }
 
-    @DeleteMapping("/{productId}")
+    @DeleteMapping("/product/{productId}")
     public ResponseEntity delete(@PathVariable String productId) {
+        LOGGER.debug("Executing delete product with id {}...", productId);
         boolean deleted = productRepository.delete(SKU.stockKeepingUnitFrom(productId));
-        return deleted ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler({IllegalArgumentException.class, DateTimeParseException.class})
-    public ResponseEntity handleBadRequestExceptions(Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity handle(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Sorry, something went wrong");
+        var response = deleted ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+        LOGGER.debug("Deleting of product with id {} finished {}", productId, deleted ? "successfully" : "unsuccessfully");
+        return response;
     }
 }
---
+++ src/main/java/com/kolotree/springproductmanagement/web/ResponseEntityExceptionResolver.java
@@ -0,0 +1,29 @@
+package com.kolotree.springproductmanagement.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.format.DateTimeParseException;
+
+@ControllerAdvice
+public class ResponseEntityExceptionResolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResponseEntityExceptionResolver.class);
+
+    @ExceptionHandler({IllegalArgumentException.class, DateTimeParseException.class})
+    public ResponseEntity<String> handleBadRequestExceptions(HttpServletRequest webRequest, Exception e) {
+        LOGGER.debug("Request {} {} failed with message {}", webRequest.getMethod(), webRequest.getRequestURI(), e.getMessage());
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception e) {
+        LOGGER.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Sorry, something went wrong");
+    }
+}
