57289be6eab21445ebebc1beb4a925e946be470c
Tijana Vislavski <tijana.vislavski@kolotree.com>
2019-10-18 15:55:03 +0200

    Initialized spring project

---
+++ src/main/java/com/kolotree/springproductmanagement/SpringProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kolotree.springproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kolotree/springproductmanagement/SpringProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolotree.springproductmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
